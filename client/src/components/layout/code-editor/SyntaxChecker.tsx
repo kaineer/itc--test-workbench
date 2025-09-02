9754c2308895c3f27256821504046bc61dd2e193
Sergey Kluchkovsky <kaineer@gmail.com>
2025-09-02 11:16:56 +0500

    Сливает изменения

---
+++ client/src/components/layout/code-editor/SyntaxChecker.tsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { codeSlice } from "@slices/code";
+import { parseWithAcorn } from "@utils/parseWithAcorn";
+
+export const SyntaxChecker = () => {
+  const { setValid } = codeSlice.actions;
+  const { getCode } = codeSlice.selectors;
+  const code = useSelector(getCode);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let valid = true;
+    try {
+      parseWithAcorn(code);
+    } catch (err) {
+      valid = false;
+    }
+
+    dispatch(setValid(valid));
+  }, [code]);
+
+  return null;
+}
