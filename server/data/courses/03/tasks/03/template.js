913eca4e059b532d415a7b88f82af940d0e9899d
Sergey Kluchkovsky <kaineer@gmail.com>
2025-09-30 17:00:30 +0500

    Убирает неиспользуемый код

--- server/data/courses/03/tasks/03/template.js
+++ server/data/courses/03/tasks/03/template.js
@@ -2,7 +2,6 @@ class Point {
   constructor(x, y) { this.move(x, y); }
   move(x, y) { this.x = x; this.y = y; }
   moveBy(x, y) { this.x += x; this.y += y; }
-  mirror(x, y) { this.x += 2 * (x - this.x); this.y += 2 * (y - this.y); }
 }
 
 class Rectangle {
